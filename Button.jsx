834a49f1a92f5a806faa040462f53d43c91c39f2
JOHN-123-CRAETOR <jkboateng019@st.ug.edu.gh>
2025-05-18 18:56:07 +0000

    UltimateButton.jsx

--- Button.jsx
+++ Button.jsx
@@ -1,27 +1,4 @@
 
-/*
-function Button(){
-
-    let count = 0;
-    const handleClick = (name) => {
-        if (count < 3){
-            count++;
-            console.log(`${name} you clicked me ${count} times`);
-        }
-        else{
-            console.log(`${name} stop clicking me!`);
-        }
-    }
-
-   
-
-    
-    return(<button onClick={() => handleClick("John")}>Click Me</button>); 
-
-}
-
-export default Button
-*/
 import React, { useState, useRef, useEffect } from "react";
 
 const CLICK_ACHIEVEMENTS = [5, 10, 20];
