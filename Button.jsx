16cd01bdd160a178aaf166098ac1decfe6a177bc
JOHN-123-CRAETOR <jkboateng019@st.ug.edu.gh>
2025-05-18 17:26:05 +0000

    Add files via upload

---
+++ Button.jsx
@@ -0,0 +1,32 @@
+
+/*
+function Button(){
+
+    let count = 0;
+    const handleClick = (name) => {
+        if (count < 3){
+            count++;
+            console.log(`${name} you clicked me ${count} times`);
+        }
+        else{
+            console.log(`${name} stop clicking me!`);
+        }
+    }
+
+   
+
+    
+    return(<button onClick={() => handleClick("John")}>Click Me</button>); 
+
+}
+
+export default Button
+*/
+
+function Button(){
+const handleClick = (e) => console.log(e)
+
+return(<button onClick = {(e) => handleClick(e)}>Click me  </button> );
+}
+
+export default Button
